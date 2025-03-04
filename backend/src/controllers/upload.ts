57538b8bbab19536da3f2ed019d3b93821b7d3ab
Qwint <Qwint69@yandex.ru>
2025-03-04 14:42:36 +0300

    file

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,7 +1,10 @@
-import BadRequestError from '../errors/bad-request-error'
 import { NextFunction, Request, Response } from 'express'
 import { constants } from 'http2'
+import { join } from 'path'
+import { writeFile, unlink } from 'fs/promises'
 import sharp from 'sharp'
+import { tmpdir } from 'os'
+import BadRequestError from '../errors/bad-request-error'
 
 export const uploadFile = async (
     req: Request,
@@ -12,30 +15,29 @@ export const uploadFile = async (
         return next(new BadRequestError('Файл не загружен'))
     }
 
+    const fileSize = req.file.size
+    if (fileSize < 2048) {
+        return next(new BadRequestError('Размер файла должен быть больше 2KB'))
+    }
+    if (fileSize > 10 * 1024 * 1024) {
+        return next(
+            new BadRequestError('Размер файла не должен превышать 10MB')
+        )
+    }
+
     try {
-        const fileSize = req.file.size
+        const image = await sharp(req.file.buffer).metadata()
+        if (!image.format) {
+            return next(new BadRequestError('Файл не является изображением'))
+        }
+    } catch {
+        return next(new BadRequestError('Ошибка при обработке изображения'))
+    }
 
-        if (fileSize < 2048) {
-            return next(
-                new BadRequestError('Размер файла должен быть больше 2KB')
-            )
-        }
-        if (fileSize > 10 * 1024 * 1024) {
-            return next(
-                new BadRequestError('Размер файла не должен превышать 10MB')
-            )
-        }
+    const tempFilePath = join(tmpdir(), req.file.filename)
 
-        try {
-            const image = await sharp(req.file.buffer).metadata()
-            if (!image.format) {
-                return next(
-                    new BadRequestError('Файл не является изображением')
-                )
-            }
-        } catch {
-            return next(new BadRequestError('Ошибка при обработке изображения'))
-        }
+    try {
+        await writeFile(tempFilePath, req.file.buffer)
 
         const filePath = process.env.UPLOAD_PATH
             ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
