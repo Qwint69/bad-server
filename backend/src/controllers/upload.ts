9a5125071247bc24ad369cae752e2e39417b9138
Qwint <Qwint69@yandex.ru>
2025-03-04 13:48:39 +0300

    files

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,10 +1,7 @@
+import BadRequestError from '../errors/bad-request-error'
 import { NextFunction, Request, Response } from 'express'
 import { constants } from 'http2'
-import { join } from 'path'
-import { stat, writeFile, unlink } from 'fs/promises'
 import sharp from 'sharp'
-import BadRequestError from '../errors/bad-request-error'
-import { tmpdir } from 'os'
 
 export const uploadFile = async (
     req: Request,
@@ -14,49 +11,40 @@ export const uploadFile = async (
     if (!req.file) {
         return next(new BadRequestError('Файл не загружен'))
     }
-    const tempFilePath = join(tmpdir(), req.file.filename)
 
     try {
-        await writeFile(tempFilePath, req.file.buffer)
+        const fileSize = req.file.size
 
-        const fileStat = await stat(tempFilePath)
-        if (fileStat.size < 2048) {
-            await unlink(tempFilePath)
+        if (fileSize < 2048) {
             return next(
                 new BadRequestError('Размер файла должен быть больше 2KB')
             )
         }
-        if (fileStat.size > 10 * 1024 * 1024) {
-            await unlink(tempFilePath)
+        if (fileSize > 10 * 1024 * 1024) {
             return next(
                 new BadRequestError('Размер файла не должен превышать 10MB')
             )
         }
 
         try {
-            const image = await sharp(tempFilePath).metadata()
+            const image = await sharp(req.file.buffer).metadata()
             if (!image.format) {
-                await unlink(tempFilePath)
                 return next(
                     new BadRequestError('Файл не является изображением')
                 )
             }
         } catch {
-            await unlink(tempFilePath)
             return next(new BadRequestError('Ошибка при обработке изображения'))
         }
 
         const filePath = process.env.UPLOAD_PATH
-            ? join('/', process.env.UPLOAD_PATH, req.file.filename)
-            : join('/', req.file.filename)
-
-        await unlink(tempFilePath)
+            ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
+            : `/${req.file.filename}`
 
         return res.status(constants.HTTP_STATUS_CREATED).json({
             fileName: filePath,
         })
     } catch (error) {
-        await unlink(tempFilePath).catch(() => null)
         return next(error)
     }
 }
